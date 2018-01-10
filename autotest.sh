64b96b14735ca821c69d9274772c3ce614e087c8
Vitaly Kamluk <username@users.noreply.github.com>
2018-01-10 10:49:16 +0800

    Autotest reliability improvement
    Removed sleep timeouts, replaced with actual expect statements, which should increase testing reliability on slower systems.

--- autotest.sh
+++ autotest.sh
@@ -15,7 +15,7 @@
 
 ISONAME="${PROJECTNAME}-16.04.iso"
 TESTLOG="./autotest.log"
-VISIBLE=1 #show tmux interface during testing
+VISIBLE=0 #show tmux interface during testing
 
 if [ ! -f "./$ISONAME" ]
 then
