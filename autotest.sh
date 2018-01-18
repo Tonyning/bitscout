83e3d5e62692c97ad4c72c653d5c6fba898ba24a
Vitaly Kamluk <username@users.noreply.github.com>
2018-01-18 14:54:40 +0800

    Autotest fix
    Set SHELL environment variable explicitly. Previously it could cause problems when started from a non-bash shell.

--- autotest.sh
+++ autotest.sh
@@ -10,6 +10,7 @@
 # A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 # You should have received a copy of the GNU General Public License along with
 # Bitscout. If not, see <http://www.gnu.org/licenses/>.
+export SHELL=/bin/bash
 
 . ./scripts/functions
 
